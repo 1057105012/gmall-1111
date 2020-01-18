999ff0af7a2a60c28ef9553bb67d3b96a9d8bc25
1057105012 <1057105012@qq.com>
2020-01-18 18:59:12 +0800

    项目逆向完成

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,28 @@
 package com.atguigu.gmall.pms;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GmallPmsApplicationTests {
-
+public class GmallPmsApplicationTests {
+	@Autowired
+	private ProductService productService;
+	@Autowired
+	private BrandService brandService;
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+//		Product byId = productService.getById(1);
+//		System.out.println(byId.getName());
+		Brand byId = brandService.getById(53);
+		System.out.println("保存成功" + byId.getName());
 	}
 
 }
